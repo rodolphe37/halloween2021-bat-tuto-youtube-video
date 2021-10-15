c9c0d21fce8a658fe7e4f6bcbade7d352ce7eb0f
rodolphe37 <rodolphe.a@gmail.com>
2021-10-15 11:35:21 +0200

    first commit-

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Bat from "./components/bat/Bat";
 
 function App() {
   return (
     <div className="App">
+      <Bat />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
